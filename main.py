7acffc73f24e0b4fde0fda600d38f846318121fa
prasad145 <acharyaprasad145@gmail.com>
2021-03-25 21:51:34 +0530

    added basic flask template

---
+++ main.py
@@ -0,0 +1,11 @@
+import sqlite3
+from flask import Flask , render_template , redirect , url_for , request , session
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(debug = True)
