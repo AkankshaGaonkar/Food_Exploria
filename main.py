9c4e2f1c852a30bdb8a685f3b2b6e68b4b4507f6
Akanksha <ashugaonkar2000@gmail.com>
2021-03-31 23:09:35 +0530

    login page

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/home")
 def index():
-    return render_template("home.html")
+    return render_template("webpagehome.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
