64028f6c41ca59d618fae8d2a8ef97c14522820e
prasad145 <acharyaprasad145@gmail.com>
2021-04-18 16:10:25 +0530

    added necessary changes

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from flask import Flask , render_template , redirect , url_for , request , sessi
 
 app = Flask(__name__)
 
+cake_names = ["Black Forest", "Choco Lava", "Red Velvet", "Ferrero Rocher"]
+prices = [300, 450, 400, 550]
+
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template("webpagehome.html")
+    return render_template("home.html", data = cake_names, prices = prices)
+    # return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
